390c4216fbdb4d05e78088fc5fa6dfcec11062b7
rac99 <rhac99@gmail.com>
2020-06-27 13:22:15 -0400

    added home page & links

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return ("Hello World")
+    return render_template("home.html")
 
 @app.route('/survey')
 def survey():
