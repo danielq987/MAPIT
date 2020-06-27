7638b884bfeec3f477ff16845e0ae18ecf9a7eaf
danielq987 <danielq987@gmail.com>
2020-06-27 12:59:48 -0400

    a few changes

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,6 +1,11 @@
 from flaskr import app
+from flask import Flask, render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     return ("Hello World")
+
+@app.route('/survey')
+def survey():
+    return render_template('survey.html')
